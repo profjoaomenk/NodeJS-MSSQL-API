6c11d030d0136eb76e2ab41bc99a5b6dc0a0cb45
profjoaomenk <59892681+profjoaomenk@users.noreply.github.com>
2023-09-01 18:42:05 -0300

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: "sqlserver-rm9999.database.windows.net",
+  HOST: "Servidor",
   PORT: "1433",
-  USER: "admsql",
-  PASSWORD: "NodeAPI@2tds",
-  DB: "dbnode",
+  USER: "Usuario",
+  PASSWORD: "Senha",
+  DB: "Banco",
   dialect: "mssql",
   pool: {
     max: 5,
